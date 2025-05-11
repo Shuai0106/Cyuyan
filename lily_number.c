d0a147e13e22900a8c65cbc9b67d63f2ec815c7c
Shuai_ <19829121053@163.com>
2025-05-11 13:07:07 +0800

    Add files via upload

--- lily_number.c
+++ lily_number.c
@@ -1,24 +1,36 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <math.h>
-int main()
+int is_lilynumber(int i)
 {
 	int n = 0;
-	for (n = 1; n <= 1000000; n++)
+	int r = i;
+	while (r)
 	{
-		if (sizeof(n) >= 3)
+		r /= 10;
+		n++;
+	}
+	r = i;
+	int j = 0;
+	int sum = 0;
+	for (j = 0; j < n; j++)
+	{
+		int k = (int)pow(i % 10, n);
+		sum += k;
+		i /= 10;
+	}
+	return sum == r;
+}
+int main()
+{
+	int i = 0;
+	for (i = 1; i <= 10000; i++)
+	{
+		if (is_lilynumber(i))
 		{
-			int si = sizeof(n);
-			int j = 0;
-			int sum = 0;
-			for (j = 1; j <= si; j++)
-			{
-				int k = (int)pow(10, j);
-				sum += (n / k) * (n % k);
-			}
-			if (sum == n)
-				printf("%d ", n);
+			printf("%d ", i);
 		}
 	}
+	
 	return 0;
 }
